c668890752bb73e9767835ca6733750a8993adcb
jandersoneusebio <jandersonturiista14@gmail.com>
2021-05-09 16:59:42 -0300

    Redução do serial

--- js/services/serialGeneratorService.js
+++ js/services/serialGeneratorService.js
@@ -3,7 +3,7 @@ angular.module("listaTelefonica").provider("serialGenerator", function(){
         return{
             generate: function(){
                 var serial = "";
-                while(serial.length < 20){
+                while(serial.length < 6){
                     serial += String.fromCharCode(Math.floor(Math.random() * 64) + 32);
                 }
                 return serial;
